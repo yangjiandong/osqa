46d473f9060cc1d0391c51a73300feccf244ea73
yangjiandong <young.jiandong@gmail.com>
2012-02-10 17:14:26 +0800

    update txt

---
+++ src/utils/parrot.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-#
+
+#显式的继承object
+class Parrot(object):
+    def __init__(self):
+        self._voltage = 100000
+
+    @property
+    def voltage(self):
+        """Get the current voltage."""
+        return self._voltage
+    
+    #配合@property,增加setter
+    @voltage.setter
+    def voltage(self, new_value):
+        self._voltage = new_value
+        
+if __name__ == "__main__":
+    # instance
+    p = Parrot()
+    # similarly invoke "getter" via @property
+    print p.voltage
+    # update, similarly invoke "setter"
+    p.voltage = 12
